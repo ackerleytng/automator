dffdd00d8a0fcd9c29fdbd2a9661a76de46b9b1e
Ackerley Tng <ackerleytng@gmail.com>
2017-09-19 21:42:26 +0800

    Removed unused files

--- automator/telnet_controller.py
+++
@@ -1,7 +0,0 @@
-from controller import Controller
-
-
-class TelnetController(Controller):
-    def __init__(self, name, shell):
-        super(TelnetController, self).__init__(shell)
-        self.name = name
--- automator/timeout.py
+++
@@ -1,3 +0,0 @@
-class Timeout(Exception):
-    def __init__(self, msg):
-        super(Timeout, self).__init__(msg)
