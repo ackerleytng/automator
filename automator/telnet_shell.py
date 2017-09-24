29819476ea611f7495f9871632ddf710766a85d9
Ackerley Tng <ackerleytng@gmail.com>
2017-09-24 10:59:44 +0800

    Made width of TelnetShell configurable at start()

--- automator/telnet_shell.py
+++ automator/telnet_shell.py
@@ -6,28 +6,34 @@ from telnetlib import DO, DONT, IAC, WILL, WONT, NAWS, SB, SE
 from network_shell import NetworkShell
 
 
-# Thank you, https://stackoverflow.com/questions/38288887/python-telnetlib-read-until-returns-cut-off-string
-def set_max_window_size(tsocket, command, option):
-    """
-    Set Window size to resolve line width issue
-    Set Windows size command: IAC SB NAWS <16-bit value> <16-bit value> IAC SE
-    --> inform the Telnet server of the window width and height.
-    Refer to https://www.ietf.org/rfc/rfc1073.txt
-    :param tsocket: telnet socket object
-    :param command: telnet Command
-    :param option: telnet option
-    :return: None
-    """
-    if command == DO and option == NAWS:
-        width = struct.pack('!H', 1024)
-        height = struct.pack('!H', 1024)
-        tsocket.send(IAC + WILL + NAWS)
-        tsocket.send(IAC + SB + NAWS + width + height + IAC + SE)
-    # -- below code taken from telnetlib source
-    elif command in (DO, DONT):
-        tsocket.send(IAC + WONT + option)
-    elif command in (WILL, WONT):
-        tsocket.send(IAC + DONT + option)
+def build_callback(width):
+    # From https://stackoverflow.com/questions/38288887/python-telnetlib-read-until-returns-cut-off-string
+    def set_max_window_size(tsocket, command, option):
+        """
+        Set Window size to resolve line width issue
+        Set Windows size command: IAC SB NAWS <16-bit value>
+                                  <16-bit value> IAC SE
+        --> inform the Telnet server of the window width and height.
+        Refer to https://www.ietf.org/rfc/rfc1073.txt
+        :param tsocket: telnet socket object
+        :param command: telnet Command
+        :param option: telnet option
+        :return: None
+        """
+        if command == DO and option == NAWS:
+            width_short = struct.pack('!H', width)
+            height_short = struct.pack('!H', 80)
+            tsocket.send(IAC + WILL + NAWS)
+            tsocket.send(IAC + SB + NAWS +
+                         width_short + height_short +
+                         IAC + SE)
+        # -- below code taken from telnetlib source
+        elif command in (DO, DONT):
+            tsocket.send(IAC + WONT + option)
+        elif command in (WILL, WONT):
+            tsocket.send(IAC + DONT + option)
+
+    return set_max_window_size
 
 
 class TelnetShell(NetworkShell):
@@ -48,10 +54,10 @@ class TelnetShell(NetworkShell):
 
         return self._shell.fileno()
 
-    def start(self):
+    def start(self, width=1024):
         self._shell = telnetlib.Telnet(self.host, self.port,
                                        self.timeout)
-        self._shell.set_option_negotiation_callback(set_max_window_size)
+        self._shell.set_option_negotiation_callback(build_callback(width))
 
         return self
 
